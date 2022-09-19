f2e68126f7f334118546ff94277567c65c84d89b
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-09-19 13:00:41 +0200

    balazs-matyassy: 3. adatok kiírása

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -3,6 +3,7 @@ package bolyai;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +39,7 @@ public class Main {
             // Hibától függetlenül lezárjuk a megnyitott fájlt a blokk végén.
             // Nincs catch blokk, emiatt nem kezeli a hibákat,
             // de nem tartjuk feleslegesen nyitva a fájlt.
+            // Ebben az esetben olvasásra nyitunk meg egy fájlt.
             try (BufferedReader reader = new BufferedReader(new FileReader("SuperBowl.txt"))) {
                 reader.readLine(); // fejléc "eldobása", nem mentjük semmilyen változóba
 
@@ -63,6 +65,35 @@ public class Main {
             // System.out.printf: formázott, NINCS sortörés
             System.out.printf("4. feladat: Döntők száma: %d\n", dontok.size());
 
+            /*
+                3. Adatok kiírása
+             */
+            // Írásra nyitunk meg egy fájlt.
+            // Itt sincs hibakezelés (ebben az esetben a külső catch kezeli az esetleges hibákat).
+            try (PrintWriter writer = new PrintWriter("SuperBowlKi.txt")) {
+                // Ha szeretnénk fejlécet:
+                writer.println("Ssz;Dátum;Győztes;Eredmény;Vesztes;Helyszín;VárosÁllam;Nézőszám");
+
+                // for-each szerkezet
+                // Egy gyűjtemény (List<T>, Set<T> vagy T[] (tömb)) minden elemét értékenként bejárunk.
+                // A feldolgozáshoz nincs szükség az indexre.
+                for (Donto donto : dontok) {
+                    // Döntő visszaalakítása sorrá a segédmetódus segítségével.
+                    // Ebben az esetben változatlan formában írjuk ki az értékeket,
+                    // de amúgy lehtőség lenne a dontobolSor metódusban egyéb formátumra is konvertálni.
+                    String sor = dontobolSor(donto);
+                    // Konvertált sor kiírása a megnyitott fájlba.
+                    // lsd. System.out.println: NINCS formázás, VAN sortörés.
+                    writer.println(sor);
+                }
+
+                // Alternatív megoldás:
+                /* for (int i = 0; i < dontok.size(); i++) {
+                    Donto donto = dontok.get(i);
+                    String sor = dontobolSor(donto);
+                    writer.println(sor);
+                } */
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -93,8 +124,18 @@ public class Main {
         return donto;
     }
 
+    /*
+        3. Adatok kiírása
+    */
     private static String dontobolSor(Donto donto) {
-        return null;
+        return donto.sorszam
+                + ";" + donto.datum
+                + ";" + donto.gyoztes
+                + ";" + donto.eredmeny
+                + ";" + donto.vesztes
+                + ";" + donto.helyszin
+                + ";" + donto.varosAllam
+                + ";" + donto.nezoszam; // számból automatikus a szöveggé konvertálás
     }
 
 }
