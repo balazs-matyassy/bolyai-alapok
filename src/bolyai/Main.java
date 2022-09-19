3b21bbf36c209680c3f1f0467be42e10dbfcfa13
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-09-19 12:25:05 +0200

    balazs-matyassy: skeleton

---
+++ src/bolyai/Main.java
@@ -0,0 +1,17 @@
+package bolyai;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static Donto sorbolDonto(String sor) {
+        return null;
+    }
+
+    private static String dontobolSor(Donto donto) {
+        return null;
+    }
+
+}
